094672ecd0ce59d478da92bdd97ddb4245c9927a
Kent Hu <kent.hu@fluxxlabs.com>
2019-12-11 23:44:46 -0800

    Complete day 5 part 1

--- day5.rb
+++ day5.rb
@@ -1,84 +1,119 @@
 #!/usr/bin/env ruby
 
-TEST_PROGRAMS = [
-  { program: [1,0,0,0,99], expected_final_memory: [2,0,0,0,99] },
-  { program: [2,3,0,3,99], expected_final_memory: [2,3,0,6,99] },
-  { program: [2,4,4,5,99,0], expected_final_memory: [2,4,4,5,99,9801] },
-  { program: [1,1,1,4,99,5,6,0,99], expected_final_memory: [30,1,1,4,2,5,6,0,99] }
-]
+INPUT = 1.freeze
 
-MAIN_PROGRAM = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 13, 1, 19, 1, 5, 19, 23, 2, 10, 23, 27, 1, 27, 5, 31, 2, 9, 31, 35, 1, 35, 5, 39, 2, 6, 39, 43, 1, 43, 5, 47, 2, 47, 10, 51, 2, 51, 6, 55, 1, 5, 55, 59, 2, 10, 59, 63, 1, 63, 6, 67, 2, 67, 6, 71, 1, 71, 5, 75, 1, 13, 75, 79, 1, 6, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 10, 91, 95, 1, 95, 9, 99, 2, 99, 13, 103, 1, 103, 6, 107, 2, 107, 6, 111, 1, 111, 2, 115, 1, 115, 13, 0, 99, 2, 0, 14, 0].freeze
+MAIN_PROGRAM = [3,225,1,225,6,6,1100,1,238,225,104,0,1102,72,20,224,1001,224,-1440,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,1002,147,33,224,101,-3036,224,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,1102,32,90,225,101,65,87,224,101,-85,224,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,1102,33,92,225,1102,20,52,225,1101,76,89,225,1,117,122,224,101,-78,224,224,4,224,102,8,223,223,101,1,224,224,1,223,224,223,1102,54,22,225,1102,5,24,225,102,50,84,224,101,-4600,224,224,4,224,1002,223,8,223,101,3,224,224,1,223,224,223,1102,92,64,225,1101,42,83,224,101,-125,224,224,4,224,102,8,223,223,101,5,224,224,1,224,223,223,2,58,195,224,1001,224,-6840,224,4,224,102,8,223,223,101,1,224,224,1,223,224,223,1101,76,48,225,1001,92,65,224,1001,224,-154,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1107,677,226,224,1002,223,2,223,1005,224,329,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,344,1001,223,1,223,1107,226,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,374,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,389,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,404,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,419,101,1,223,223,1008,226,677,224,1002,223,2,223,1006,224,434,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,449,101,1,223,223,1108,677,226,224,102,2,223,223,1006,224,464,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,479,101,1,223,223,7,226,677,224,1002,223,2,223,1006,224,494,1001,223,1,223,7,677,677,224,102,2,223,223,1006,224,509,101,1,223,223,107,226,677,224,1002,223,2,223,1006,224,524,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,539,1001,223,1,223,108,677,226,224,102,2,223,223,1005,224,554,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1006,224,584,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,599,1001,223,1,223,1007,677,226,224,1002,223,2,223,1005,224,614,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,629,101,1,223,223,1108,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,8,226,677,224,1002,223,2,223,1006,224,659,101,1,223,223,107,226,226,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226].freeze
+
+class Instruction
+  attr_reader :opcode, :parameter_modes, :parameters, :command, :length
+
+  DEFAULT_PARAMETER_MODE = 0.freeze
+
+  def initialize(opcode, parameter_modes, parameters)
+    @opcode = opcode
+    @parameter_modes = parameter_modes
+    @parameters = parameters
+    @command = Computer::OPCODES[opcode][:command]
+    @length = Computer::OPCODES[opcode][:instruction_length]
+  end
+
+  def parameter(position)
+    @parameters[position]
+  end
+
+  def parameter_mode(position)
+    @parameter_modes[position] || DEFAULT_PARAMETER_MODE
+  end
+
+  # keep in mind default of zero for parameter_modes!!!
+end
 
 class Computer
-  attr_accessor :memory, :default_memory
+  attr_accessor :memory
 
   OPCODES = {
-    1 => :+,
-    2 => :*,
-    99 => :halt
+    1 =>  { command: :add,      instruction_length: 4 },
+    2 =>  { command: :multiply, instruction_length: 4 },
+    3 =>  { command: :save,     instruction_length: 2 },
+    4 =>  { command: :output,   instruction_length: 2 },
+    99 => { command: :halt,     instruction_length: 1 }
   }.freeze
 
   def initialize(program)
-    self.default_memory = program.dup
-    self.memory = default_memory.dup
+    self.memory = program.dup
   end
 
-  def reset_memory
-    self.memory = default_memory.dup
+  def parse_instruction(instruction_ptr)
+    opcode_and_parameter_modes = memory[instruction_ptr]
+
+    # Get opcode from rightmost two digits
+    opcode = opcode_and_parameter_modes % 100
+
+    # Get parameter modes from remaining digits
+    parameter_modes_reversed = opcode_and_parameter_modes / 100
+    parameter_modes = parameter_modes_reversed.to_s.reverse.split('').map(&:to_i)
+
+    # Get parameters from remaining values in instruction
+    instruction_length = OPCODES[opcode][:instruction_length]
+    parameters = memory[instruction_ptr + 1..instruction_ptr + instruction_length - 1]
+
+    Instruction.new(opcode, parameter_modes, parameters)
+  end
+    
+  def get_operand(instruction, parameter_position)
+    parameter = instruction.parameter(parameter_position)
+    parameter_mode = instruction.parameter_mode(parameter_position)
+    case parameter_mode
+    when 0
+      memory[parameter]
+    when 1
+      parameter
+    end
   end
 
-  def get_command(address)
-    opcode = memory[address]
-    OPCODES[opcode]
+  def execute_instruction(instruction)
+    case instruction.command
+    when :add
+      operand1 = get_operand(instruction, 0)
+      operand2 = get_operand(instruction, 1)
+      output_address = instruction.parameters[2]
+      #puts "Adding: #{operand1} + #{operand2}, store at #{output_address}"
+      memory[output_address] = operand1 + operand2
+    when :multiply
+      operand1 = get_operand(instruction, 0)
+      operand2 = get_operand(instruction, 1)
+      output_address = instruction.parameters[2]
+      memory[output_address] = operand1 * operand2
+    when :save
+      output_address = instruction.parameters[0]
+      memory[output_address] = INPUT
+    when :output
+      operand1 = get_operand(instruction, 0)
+      puts operand1
+    end
   end
 
-  # Execute operation at address, then return new address
-  def execute_operation(address)
-    command = get_command(address)
-    operand1_address = memory[address + 1]
-    operand2_address = memory[address + 2]
-    output_address = memory[address + 3]
-    operand1 = memory[operand1_address]
-    operand2 = memory[operand2_address]
-    memory[output_address] = operand1.send(command, operand2)
-    address + 4
-  end
-
-  def execute_program(noun = nil, verb = nil)
-    memory[1] = noun if noun
-    memory[2] = verb if verb
+  def execute_program
     instruction_ptr = 0
     loop do
-      break if get_command(instruction_ptr) == :halt
-      instruction_ptr = execute_operation(instruction_ptr)
+      instruction = parse_instruction(instruction_ptr)
+      break if instruction.command == :halt
+
+      # Execute the command
+      # puts "Instruction:"
+      # puts "  opcode: #{instruction.opcode}"
+      # puts "  parameter_modes: #{instruction.parameter_modes}"
+      # puts "  parameters: #{instruction.parameters}"
+      # puts "  command: #{instruction.command}"
+      # puts "  length: #{instruction.length}"
+      execute_instruction(instruction)
+
+      instruction_ptr += instruction.length
     end
-    memory[0]
-  end
-
-  def memory_matches?(memory_to_compare)
-    memory == memory_to_compare
-  end
-end
-
-# Run test programs
-TEST_PROGRAMS.each do |test_case|
-  program = test_case[:program]
-  expected_final_memory = test_case[:expected_final_memory]
-  computer = Computer.new(program)
-  computer.execute_program
-  unless computer.memory_matches?(expected_final_memory)
-    puts "Invalid final memory for program #{test_case[:program]}"
+    # memory[0]
   end
 end
 
 # Part 1
 computer = Computer.new(MAIN_PROGRAM)
-puts computer.execute_program(12, 2)
-
-# Part 2
-(0..99).each do |noun|
-  (0..99).each do |verb|
-    computer.reset_memory
-    puts noun * 100 + verb if computer.execute_program(noun, verb) == 19690720
-  end
-end
+puts computer.execute_program()
