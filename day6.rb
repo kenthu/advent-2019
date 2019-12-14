5f524edb3038843a6a1ffc87726e9b7b8c3be82e
Kent Hu <kent.hu@fluxxlabs.com>
2019-12-14 00:37:44 -0800

    Complete day 6

--- day6.rb
+++ day6.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
-CENTER_OF_MASS_VALUE = 'COM'.freeze
-TEST_MAP = %w[COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L].freeze
+ROOT_VALUE = 'COM'.freeze  # COM == Center Of Mass
+TEST_MAP_PART_1 = %w[COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L].freeze
+TEST_MAP_PART_2 = %w[COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN].freeze
 
 class TreeNode
-  attr_reader :value, :children
+  attr_reader :value, :children, :parent
+
+  class ParentAlreadySetError < StandardError; end
 
   def initialize(value)
     @value = value
@@ -13,6 +16,21 @@ class TreeNode
 
   def add_child(child_node)
     @children << child_node
+  end
+
+  def set_parent(parent_node)
+    raise ParentAlreadySetError if @parent
+    @parent = parent_node
+  end
+
+  def ancestors
+    my_ancestors = []
+    current_node = self
+    while (parent = current_node.parent)
+      my_ancestors.unshift(parent)
+      current_node = parent
+    end
+    my_ancestors
   end
 
   def print
@@ -35,7 +53,8 @@ class Tree
     parent_node = @node_hash[parent_value]
     child_node = @node_hash[child_value]
     parent_node.add_child(child_node)
-    @root = parent_node if !@root && parent_value == CENTER_OF_MASS_VALUE
+    child_node.set_parent(parent_node)
+    @root = parent_node if !@root && parent_value == ROOT_VALUE
   end
 
   # Return sum of depths. Execute breadth-first tree traversal
@@ -53,13 +72,30 @@ class Tree
     sum_of_depths
   end
 
+  def self.closest_common_ancestor(ancestors1, ancestors2)
+    ancestors1.reverse_each do |ancestor|
+      return ancestor if ancestors2.include?(ancestor)
+    end
+  end
+
+  # Find minimum number of orbital transfers required to move from the object
+  # YOU are orbiting to the object SAN is orbiting
+  def transfers_to_reach_santa
+    santa_ancestors = @node_hash['SAN'].ancestors
+    you_ancestors = @node_hash['YOU'].ancestors
+    closest_common_ancestor = self.class.closest_common_ancestor(santa_ancestors, you_ancestors)
+    distance_from_common_to_santa = santa_ancestors.length - santa_ancestors.index(closest_common_ancestor) - 1
+    distance_from_common_to_you = you_ancestors.length - you_ancestors.index(closest_common_ancestor) - 1
+    distance_from_common_to_santa + distance_from_common_to_you
+  end
+
   def print
     @root.print if @root
   end
 end
 
 # Part 1 - Test
-test_tree = Tree.new(TEST_MAP)
+test_tree = Tree.new(TEST_MAP_PART_1)
 puts test_tree.orbit_count
 # 42
 
@@ -68,3 +104,12 @@ map = File.readlines('day6_map.txt').map(&:strip)
 tree = Tree.new(map)
 puts tree.orbit_count
 # 204521
+
+# Part 2 - Test
+test_tree_2 = Tree.new(TEST_MAP_PART_2)
+puts test_tree_2.transfers_to_reach_santa
+# 4
+
+# Part 2 - Execute
+puts tree.transfers_to_reach_santa
+# 307
